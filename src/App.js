cc6de8bb3717d50e6fa5f72e833bea9f40f000e2
DragunDev <plarisanmarc@gmail.com>
2025-06-06 21:35:25 +0800

    Complete grade table

--- src/App.js
+++ src/App.js
@@ -27,13 +27,18 @@ function Header() {
 }
 
 function SubjectAverageCalculator() {
-  const [averageGrade, setAverageGrade] = useState(0);
-  const [prelimsGrade, setPrelimsGrade] = useState(0);
-  const [midtermsGrade, setMidtermsGrade] = useState(0);
-  const [prefinalsGrade, setPrefinalsGrade] = useState(0);
-  const [finalsGrade, setFinalsGrade] = useState(0);
+  const [averageGrade, setAverageGrade] = useState(null);
+  const [prelimsGrade, setPrelimsGrade] = useState(null);
+  const [midtermsGrade, setMidtermsGrade] = useState(null);
+  const [prefinalsGrade, setPrefinalsGrade] = useState(null);
+  const [finalsGrade, setFinalsGrade] = useState(null);
 
   function calculateAverageGrade() {
+    if (!prelimsGrade || !midtermsGrade || !prefinalsGrade || !finalsGrade) {
+      // show modal
+      return;
+    }
+
     const calculatedAverageGrade =
       prelimsGrade * 0.2 +
       midtermsGrade * 0.2 +
@@ -44,7 +49,7 @@ function SubjectAverageCalculator() {
 
   return (
     <Card className="shadow-sm mb-4 full-width">
-      <Card.Header as="h5">Enter Your Grades {averageGrade}</Card.Header>
+      <Card.Header as="h5">Enter Your Grades</Card.Header>
       <Card.Body>
         <Form>
           <Row className="mb-3">
@@ -89,14 +94,88 @@ function SubjectAverageCalculator() {
             Calculate Average GWA
           </Button>
         </Form>
+        {averageGrade !== null && SubjectAverageResult(averageGrade)}
       </Card.Body>
     </Card>
   );
 }
 
-function SubjectAverageResult() {}
+function SubjectAverageResult({ averageGradeResult }) {
+  return <p>{averageGradeResult}</p>;
+}
 
-function GradeTable() {}
+function GradeTable() {
+  return (
+    <Accordion className="mb-4">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Grade Conversion Table</Accordion.Header>
+        <Accordion.Body>
+          <Table striped bordered>
+            <thead>
+              <tr>
+                <th>Grade Range</th>
+                <th>GWA</th>
+                <th>Remarks</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>97.50 - 100.00</td>
+                <td>1.00</td>
+                <td>Excellent</td>
+              </tr>
+              <tr>
+                <td>94.50 - 97.49</td>
+                <td>1.25</td>
+                <td>Very Good</td>
+              </tr>
+              <tr>
+                <td>91.50 - 94.49</td>
+                <td>1.50</td>
+                <td>Very Good</td>
+              </tr>
+              <tr>
+                <td>88.50 - 91.49</td>
+                <td>1.50</td>
+                <td>Very Good</td>
+              </tr>
+              <tr>
+                <td>85.50 - 88.49</td>
+                <td>2.00</td>
+                <td>Satisfactory</td>
+              </tr>
+              <tr>
+                <td>81.50 - 85.49</td>
+                <td>2.25</td>
+                <td>Satisfactory</td>
+              </tr>
+              <tr>
+                <td>77.50 - 81.49</td>
+                <td>2.50</td>
+                <td>Satisfactory</td>
+              </tr>
+              <tr>
+                <td>73.50 - 77.49</td>
+                <td>2.75</td>
+                <td>Fair</td>
+              </tr>
+              <tr>
+                <td>69.50 - 73.49</td>
+                <td>3.00</td>
+                <td>Fair</td>
+              </tr>
+              <tr>
+                <td>69.40 and below</td>
+                <td>2.75</td>
+                <td>Failed</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
 
 function Footer() {}
 
